dd0ffbfa5ec576ac86b33f8ad59865b08921e19f
SCSnowden06 <SCSnowden06@gmail.com>
2025-06-04 13:45:18 -0400

    completed ch1l2

--- main.py
+++ main.py
@@ -1,18 +1,8 @@
-def stylize_title(document):
-    return add_border(center_title(document))
-
-
-# Don't touch below this line
-
-
-def center_title(document):
-    width = 40
-    title = document.split("\n")[0]
-    centered_title = title.center(width)
-    return document.replace(title, centered_title)
-
-
-def add_border(document):
-    title = document.split("\n")[0]
-    border = "*" * len(title)
-    return document.replace(title, title + "\n" + border)
+def add_prefix(document, documents):
+    prefix = f"{len(documents)}. "
+    new_doc = prefix + document
+    new_tuple = prefix + document
+    new_new_tuple = (new_tuple, )
+    # documents(new_doc)
+    return documents + new_new_tuple
+    
