49feff79aca671d63d3ef6d9066cff16f1a88c53
SCSnowden06 <SCSnowden06@gmail.com>
2025-06-04 14:38:17 -0400

    completed ch1l9

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-def get_median_font_size(font_sizes):
-    sorted_list = sorted(font_sizes)
-    if len(sorted_list) == 0:
-        return None
-    elif len(sorted_list) %2 == 0:
-        return sorted_list[((len(sorted_list) // 2) -1)]
-    else: 
-        return sorted_list[(len(sorted_list) // 2)]
+def format_line(line):
+    stripped = line.strip()
+    uppered = stripped.upper()
+    replaced = uppered.replace('.', '')
+    suffixed = replaced + '...'
+    return suffixed
+    
     
     
