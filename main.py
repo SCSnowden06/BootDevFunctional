4c031ca5a37bc5f1b482f6775deb723d0c8f95e4
Samuel <SCSnowden06@gmail.com>
2025-06-16 00:00:05 -0400

    lambda is now working, have not yet added UX

--- main.py
+++ main.py
@@ -19,17 +19,22 @@ test_tuple = [
 
 def file_type_getter(file_extension_tuples):
     dict1 = {}
-    i = 1
-
+    
     for tuple in file_extension_tuples:
         for item in tuple:
             for thing in item:                              #thing is a tuple; e.g. ('markup', ['.html', '.xml'])
                 for piece in thing[1]:
-                    #print(piece)
-                    #print(f"this is a thing0 {thing[0]} \n")
-                    dict1.update({piece : thing[0]})
+                    dict1.update({piece : thing[0]})        # .html is file_extension_tuples.tuple.item.thing[1].piece
 
     return(dict1)
  
+#def get_with_lambda(dictionary, key): {
+#    dictionary.get(key, "Unknown")
+#}
+
+lambda_function = lambda dictionary, key : dictionary.get(key, "Unknown")
 
 print(file_type_getter(test_tuple))
+
+#print(get_with_lambda(file_type_getter(test_tuple), ".py"))
+print(lambda_function(file_type_getter(test_tuple), ".docx"))
