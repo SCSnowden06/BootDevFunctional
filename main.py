83480a1697726cc5eaec0c99436aa87cf6591f04
Samuel <SCSnowden06@gmail.com>
2025-06-16 19:05:57 -0400

    cleaned things up

--- main.py
+++ main.py
@@ -1,15 +1,8 @@
 def change_bullet_style(document):
-
-    #print(f"this part is the document\n{document}\n\n")
     split_doc = document.split("\n")
-    #print(split_doc)
-    #print(list(map(convert_line, split_doc)))
-
     joined_string = "\n".join(list(map(convert_line, split_doc)))
-    #print(f"this part is thie joined string\n{joined_string}")
-    #print("\n".join(converted_document))
     return joined_string
-    #return converted_document.join("\n")
+    
 
 
 
