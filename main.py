00f77f2ca29575b0fe38158add1c7b303e58db26
Samuel <SCSnowden06@gmail.com>
2025-06-15 23:14:35 -0400

    I now have taken the input and returned dictionary with docItem and fileType. It took way too long, but was valuable for learning.

--- main.py
+++ main.py
@@ -1,3 +1,35 @@
+test_tuple = [
+    (
+        [("document", [".doc", ".docx"]), ("image", [".jpg", ".png"])],
+        
+    ),
+    (
+        [("document", [".doc", ".docx"]), ("image", [".jpg", ".png"])],
+        
+    ),
+    (
+        [("document", [".doc", ".docx"]), ("image", [".jpg", ".png"])],
+        
+    ),
+    (
+        [("code", [".py", ".js"]), ("markup", [".html", ".xml"])],
+        
+    ),
+]
+
 def file_type_getter(file_extension_tuples):
-    pass
+    dict1 = {}
+    i = 1
+
+    for tuple in file_extension_tuples:
+        for item in tuple:
+            for thing in item:                              #thing is a tuple; e.g. ('markup', ['.html', '.xml'])
+                for piece in thing[1]:
+                    #print(piece)
+                    #print(f"this is a thing0 {thing[0]} \n")
+                    dict1.update({piece : thing[0]})
+
+    return(dict1)
  
+
+print(file_type_getter(test_tuple))
