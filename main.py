3e9db33871c79daf623a0f93cbd5cd70ee422b1f
SCSnowden06 <SCSnowden06@gmail.com>
2025-06-17 18:18:41 -0400

    changes12

--- main.py
+++ main.py
@@ -17,11 +17,16 @@
 def remove_invalid_lines(document):
     #print(document)
     iterable_document = document.split("\n")
+    iterated_document = []
     for line in iterable_document:
-        print(f"look for this \n{line}\n")
-        if line.startswith("-"):
-            print("this logic is doing something") 
-        return iterable_document
+        #print(line)
+        if line.startswith("*"):
+            iterated_document.append(line)
+        #print(f"this is the iterated one \n{iterated_document}\n\n")
+    joined_string = "\n".join(iterated_document)
+    #print(f"this one is joined \n{joined_string}")
+    return joined_string
+    
     #print(iterable_document)
     #print(f"this is iterable document \n {iterable_document}")
     pass
