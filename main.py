80d0b688d6462aac516d77214bafc68447741380
Samuel <SCSnowden06@gmail.com>
2025-06-17 21:35:27 -0400

    correcting numbering paths

--- main.py
+++ main.py
@@ -1,33 +1,8 @@
-#def change_bullet_style(document):
-#    split_doc = document.split("\n")
-#    joined_string = "\n".join(list(map(convert_line, split_doc)))
-#    return joined_string
-    
-
-#def convert_line(line):
-#    old_bullet = "-"
-#    new_bullet = "*"
-#    if len(line) > 0 and line[0] == old_bullet:
-#        return new_bullet + line[1:]
-#    return line
-################################################################################
-
-
-
 def remove_invalid_lines(document):
-    #print(document)
     iterable_document = document.split("\n")
-    print(iterable_document)
     iterated_document = []
     for line in iterable_document:
-        #print(line)
         if line.startswith("-") == False:
             iterated_document.append(line)
-        #print(f"this is the iterated one \n{iterated_document}\n\n")
     joined_string = "\n".join(iterated_document)
-    #print(f"this one is joined \n{joined_string}")
     return joined_string
-    
-    #print(iterable_document)
-    #print(f"this is iterable document \n {iterable_document}")
-    pass
